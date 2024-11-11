8ba507c4b4ac38d8dda8d06ac97f77fd0d5f2351
PasinduMshan <madushan01722@gmail.com>
2024-11-11 15:39:52 +0530

    first commit

---
+++ src/main/java/lk/ijse/GreenShadowCropMonitor_BackEnd/GreenShadowCropMonitorBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.GreenShadowCropMonitor_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShadowCropMonitorBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenShadowCropMonitorBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/GreenShadowCropMonitor_BackEnd/GreenShadowCropMonitorBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.GreenShadowCropMonitor_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowCropMonitorBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
