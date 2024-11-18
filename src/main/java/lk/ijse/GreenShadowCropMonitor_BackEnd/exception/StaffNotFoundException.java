c4403541241ea507bb4be59175ce2fe9c90c4fe6
PasinduMshan <madushan01722@gmail.com>
2024-11-18 15:27:13 +0530

    create StaffNotFoundException class

---
+++ src/main/java/lk/ijse/GreenShadowCropMonitor_BackEnd/exception/StaffNotFoundException.java
@@ -0,0 +1,7 @@
+package lk.ijse.GreenShadowCropMonitor_BackEnd.exception;
+
+public class StaffNotFoundException extends RuntimeException {
+    public StaffNotFoundException() {}
+    public StaffNotFoundException(String message) {}
+    public StaffNotFoundException(String message, Throwable cause) {}
+}
